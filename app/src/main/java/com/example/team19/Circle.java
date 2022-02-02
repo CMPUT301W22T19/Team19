5c3f0ffccd450df289fdd39c1e469a29bf3f0788
Austin Tralnberg <atralnberg@gmail.com>
2022-02-02 16:11:31 -0700

    Added constructor to circle

--- app/src/main/java/com/example/team19/Circle.java
+++ app/src/main/java/com/example/team19/Circle.java
@@ -1,2 +1,8 @@
-package com.example.team19;public class Circle {
+package com.example.team19;
+
+public class Circle extends Shape {
+
+    public Circle(int x, int y) {
+        super(x, y);
+    }
 }
