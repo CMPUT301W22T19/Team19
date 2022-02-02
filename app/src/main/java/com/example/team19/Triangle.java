acdf5b45dd1dfb657d0a3effb9bd9e58663cca4d
Noah Batiuk <batiuk@ualberta.ca>
2022-02-02 16:48:10 -0700

    triangle 2

---
+++ app/src/main/java/com/example/team19/Triangle.java
@@ -0,0 +1,12 @@
+package com.example.team19;
+
+public class Triangle extends Shape {
+
+    private int w;
+    private int h;
+
+    public Triangle(int w, int h) {
+        this.w = w;
+        this.h = h;
+    }
+}
