115fb6a25c6d947204396b2ac278e75ce9be4081
Noah Batiuk <batiuk@ualberta.ca>
2022-02-02 17:10:55 -0700

    blue

--- app/src/main/java/com/example/team19/Shape.java
+++ app/src/main/java/com/example/team19/Shape.java
@@ -5,6 +5,7 @@ public abstract class Shape {
     private String color = "$PUT_YOUR_CHOICE_OF_COLOR";
     private int x;
     private int y;
+    String color "blue";
 
     public Shape(int x, int y) {
         this.x = x;
